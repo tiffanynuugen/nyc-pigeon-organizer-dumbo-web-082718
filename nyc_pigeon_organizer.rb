b1068eca0e4b095dd6c789b9962d9ff35f530dbe
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-24 03:29:39 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,20 +18,3 @@ data[:color].each do |attribute, name_array|
       end
     end
   end
-
-  data[:gender].each do |attribute, name_array|
-    name_array.each do |name|
-      if data[:gender][attribute].include?(name)
-        new_pigeon_data[name][:gender] << attribute.to_s
-      end
-    end
-  end
-
-  data[:lives].each do |attribute, name_array|
-    name_array.each do |name|
-      if data[:lives][attribute].include?(name)
-        new_pigeon_data[name][:lives] << attribute
-      end
-    end
-  new_pigeon_data
-end
