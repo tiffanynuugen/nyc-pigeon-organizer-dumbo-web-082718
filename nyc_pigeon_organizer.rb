4bd8679ec75cbac958c545a0a4b8b7b9e1aa8c8d
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-24 03:22:34 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,12 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |info, description|
-    description.each do |attribute, name|
-    binding.pry
-    new_hash << data["name"]
+	 new_pigeon_data = {}
+   data.each do |info, description|
+    description.each do |attribute, name_array|
+      name_array.each do |name|
+        binding.pry
+        new_pigeon_data[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
   end
- end
-end
