20198a7a2c152c518bcef7172fe5d8d1b923de63
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-24 03:40:44 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,24 +1,23 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-	 new_pigeon_data = {}
-   data.each do |info, description|
-    description.each do |attribute, name_array|
+  new_pigeon_data = {}
+   data.each do |attribute_category, attribute_data_hash|
+    attribute_data_hash.each do |attribute, name_array|
       name_array.each do |name|
-        new_pigeon_data[name] = {:color => [], :gender => [], :lives => []}
+        new_pigeon_data[name] = {:color => [], :gender => [], :lives => []}  
       end
     end
   end
-end
-
-data[:color].each do |attribute, name_array|
+  
+  data[:color].each do |attribute, name_array|
     name_array.each do |name|
       if data[:color][attribute].include?(name)
         new_pigeon_data[name][:color] << attribute.to_s
       end
     end
   end
-
+  
   data[:gender].each do |attribute, name_array|
     name_array.each do |name|
       if data[:gender][attribute].include?(name)
@@ -26,12 +25,14 @@ data[:color].each do |attribute, name_array|
       end
     end
   end
-
+  
   data[:lives].each do |attribute, name_array|
     name_array.each do |name|
       if data[:lives][attribute].include?(name)
         new_pigeon_data[name][:lives] << attribute
       end
     end
+  end
+  binding.pry
   new_pigeon_data
 end
