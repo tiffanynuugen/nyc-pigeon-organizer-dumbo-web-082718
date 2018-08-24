3aefef42c20297e46141167575981d186256d588
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-24 03:16:48 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   data.each do |info, description|
     description.each do |attribute, name|
     binding.pry
-    new_hash << description["name"]
+    new_hash << data["name"]
   end
  end
 end
