fee2df2ca67d4a0649fe2af1c4c9f1dc20b7e63a
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-24 03:43:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_data = {}
+   data.each do |attribute_category, attribute_data_hash|
+    attribute_data_hash.each do |attribute, name_array|
+      name_array.each do |name|
+        new_pigeon_data[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+
+  data[:color].each do |attribute, name_array|
+    name_array.each do |name|
+      if data[:color][attribute].include?(name)
+        new_pigeon_data[name][:color] << attribute.to_s
+      end
+    end
+  end
+
+  data[:gender].each do |attribute, name_array|
+    name_array.each do |name|
+      if data[:gender][attribute].include?(name)
+        new_pigeon_data[name][:gender] << attribute.to_s
+      end
+    end
+  end
+
+  data[:lives].each do |attribute, name_array|
+    name_array.each do |name|
+      if data[:lives][attribute].include?(name)
+        new_pigeon_data[name][:lives] << attribute
+      end
+    end
+  end
+  new_pigeon_data
 end
